4adee5e3a3eb172deeaa69689c575569256741ed
varoon1205 <varoon1205@gmail.com>
2026-06-10 17:23:34 -0400

    Add files via upload

---
+++ sampleprivate.py
@@ -0,0 +1,9 @@
+class myClass:
+    __privateVar = 27;
+    def __privMeth(self):
+        print("I'm inside class MYclass")
+    def hello(self):
+        print("Private Variable value: ",myClass.__privateVar)
+foo = myClass()
+foo.hello()
+foo.__privMeth
